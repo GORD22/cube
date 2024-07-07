4f60cf610cb1ad5631005ea816a0b0dde3f32eb2
GORD22 <andreyanov.15@mail.ru>
2024-07-07 09:43:33 +0300

    Настройка проекта

---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+import { MainPage } from "@/pages/main";
+import "./styles/app.scss";
+
+export const App = () => {
+  return (
+    <div className="container">
+      <MainPage />
+    </div>
+  );
+};
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,5 @@
+export const MainPage = () => {
+  return (
+    <div>Whereas recognition of the inherent dignity</div>
+  )
+}
