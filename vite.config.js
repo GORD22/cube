1568c6b4333b84607b0611915a30b1bb4ae0e4d3
GORD22 <andreyanov.15@mail.ru>
2024-07-08 12:04:12 +0300

    Настройка для деплоя на github pages

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import path from "path";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cube",
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: "/cube",
+    plugins: [react(), svgr()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
