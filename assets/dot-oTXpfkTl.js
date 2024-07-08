85fa8c8ca698b1797e159de2cf2e7dea619cb134
GORD22 <andreyanov.15@mail.ru>
2024-07-08 12:15:03 +0300

    Updates

--- assets/dot-oTXpfkTl.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-DGUMzT2i.js";const o=e=>t.createElement("svg",{id:"dot",viewBox:"0 0 16 16",xmlns:"http://www.w3.org/2000/svg",...e},t.createElement("circle",{cx:8.04818,cy:8.0484,r:7.33871,fill:"currentColor"}));export{o as default};
