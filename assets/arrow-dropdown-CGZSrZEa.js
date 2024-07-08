6b1014aebc9201cdade4a66a6a922c621c7f1865
GORD22 <andreyanov.15@mail.ru>
2024-07-08 12:05:45 +0300

    Updates

--- assets/arrow-dropdown-CGZSrZEa.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-BBmF5VXU.js";const t=o=>r.createElement("svg",{id:"arrow-dropdown",viewBox:"0 0 10 6",fill:"none",xmlns:"http://www.w3.org/2000/svg",...o},r.createElement("path",{d:"M1 1L5 5L9 1",stroke:"currentColor",strokeWidth:1.33333,strokeLinecap:"round",strokeLinejoin:"round"}));export{t as default};
--- assets/cross-BWih5tQr.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-BBmF5VXU.js";const L=t=>r.createElement("svg",{id:"cross",viewBox:"0 0 15 14",xmlns:"http://www.w3.org/2000/svg",...t},r.createElement("path",{d:"M14.8926 1.41L13.4226 0L7.59502 5.59L1.76742 0L0.297485 1.41L6.12509 7L0.297485 12.59L1.76742 14L7.59502 8.41L13.4226 14L14.8926 12.59L9.06496 7L14.8926 1.41Z",fill:"currentColor"}));export{L as default};
